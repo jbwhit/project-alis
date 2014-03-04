e36dc51f578d0ba017fdb0453d77d62e6459704b
ryancooke86@gmail.com <ryancooke86@gmail.com@3a329863-a49f-7599-527f-05673008d94c>
2014-03-04 03:26:52 +0000

    removed debugging mode

--- trunk/src/alplot.py
+++ trunk/src/alplot.py
@@ -179,12 +179,17 @@ def plot_drawplots(pages, wfemcarr, pgcnt, disp, dims, argflag, labels=None, num
 		fig[pgnum].subplots_adjust(hspace=0.1, wspace=0.1, bottom=0.07, top=0.98, left=0.04, right=0.98)
 		for i in range(pgcnt[pg]):
 			w = np.where(wfemcarr[3][pg][i] > -0.5)
-			modl_min, modl_max = np.min(wfemcarr[3][pg][i][w]), np.max(wfemcarr[3][pg][i][w])
-			flue_med = 3.0*np.median(wfemcarr[2][pg][i])
-			res_size = 0.05*(modl_max-modl_min)
-			shift = np.min([modl_min-2.0*res_size, np.min(wfemcarr[5][pg][i])-np.max(wfemcarr[2][pg][i][w])-2.0*res_size])
 			if np.size(w[0]) == 0:
-				msgs.warn("There was no model data found for a panel", verbose=argflag['out']['verbose'])
+#				msgs.warn("There was no model data found for plot page {0:d}, panel {1:d}".format(pg+1,i+1), verbose=argflag['out']['verbose'])
+				modl_min, modl_max = np.min(wfemcarr[1][pg][i]), np.max(wfemcarr[1][pg][i])
+				flue_med = 3.0*np.median(wfemcarr[2][pg][i])
+				res_size = 0.05*(modl_max-modl_min)
+				shift = np.min([modl_min-2.0*res_size, np.min(wfemcarr[5][pg][i])-np.max(wfemcarr[2][pg][i])-2.0*res_size])
+			else:
+				modl_min, modl_max = np.min(wfemcarr[3][pg][i][w]), np.max(wfemcarr[3][pg][i][w])
+				flue_med = 3.0*np.median(wfemcarr[2][pg][i])
+				res_size = 0.05*(modl_max-modl_min)
+				shift = np.min([modl_min-2.0*res_size, np.min(wfemcarr[5][pg][i])-np.max(wfemcarr[2][pg][i][w])-2.0*res_size])
 			ax = fig[pgnum].add_subplot(dims[0],dims[1],i+1)
 			# Plot the error spectrum
 			ax.fill_between(wfemcarr[0][pg][i],wfemcarr[5][pg][i]-wfemcarr[2][pg][i],wfemcarr[5][pg][i]+wfemcarr[2][pg][i],facecolor='0.7')
@@ -217,7 +222,7 @@ def plot_drawplots(pages, wfemcarr, pgcnt, disp, dims, argflag, labels=None, num
 				wmin=1.3*np.min(wfemcarr[0][pg][i][w])-0.3*np.mean(wfemcarr[0][pg][i][w])
 				wmax=1.3*np.max(wfemcarr[0][pg][i][w])-0.3*np.mean(wfemcarr[0][pg][i][w])
 			else:
-				msgs.warn("No model to plot for page {0:d} panel {1:d}".format(pg+1+numpages,i+1)+msgs.newline()+"Check the fitrange for this parameter?", verbose=verbose)
+				msgs.warn("No model to plot for page {0:d} panel {1:d}".format(pg+1+numpages,i+1)+msgs.newline()+"You might need to check the fitrange for this parameter?", verbose=verbose)
 				wmin=np.min(wfemcarr[0][pg][i])
 				wmax=np.max(wfemcarr[0][pg][i])
 #			for j in range(0,len(comparr[pg][i])/3):
